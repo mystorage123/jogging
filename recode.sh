40f591568e01e542d79221c520d87ed8fac6aba2
BAKALCALONHACKERDANPRESIDENAMIN <46281651+BAKALCALONHACKERDANPRESIDENAMIN@users.noreply.github.com>
2019-01-01 11:52:46 +0700

    Update recode.sh

--- recode.sh
+++ recode.sh
@@ -1,850 +1,437 @@
 #week 
 #dibuat penuh kasih dan sayang
 
-
 clear
-
 white='\033[37;1m'
-
 blue='\033[34;1m'
-
 green='\033[32;1m'
-
 cyan='\033[36;1m'
-
 red='\033[31;1m'
-
 yellow='\033[33;1m'
-
 purple='\033[35;1m'
-
 echo "\033[33;1m Kumpulan Tools termux By YGM Corporation"
-
 echo "\033[31;1m__________________________________________________"
-
 echo "\033[32;1m  yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo "\033[32;1m  yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo "\033[32;1m  yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo "\033[32;1m         yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo "\033[32;1m  yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo ""
-
 echo "\033[35;1m             Powered by YGM Corporation"
-
 echo "\033[31;1m__________________________________________________"
-
 echo "\033[33;1m    KUMPULAN TOOLS TERMUX BY YGM CORPORATION"
-
 echo "\033[33;1m     NOTE: GUNAKAN TOOLS INI SEBAIK-BAIKNYA"
-
 echo "\033[33;1m         © Copyright by YGM Corporation"
-
 echo "\033[31;1m__________________________________________________"
-
 echo ""
-
 echo "\033[34;1m Tanggal:" 
-
    date +"%d-%m-%y"
-
 echo ""
-
 echo "\033[37;1mSilahkan Pilih:"
-
 echo ""
-
 echo $red"-----------------------------"
-
 echo $cyan"1).Tools untuk ddos"
-
 echo $red"-----------------------------"
-
 echo $cyan"2).Bermain moon buggy"
-
 echo $red"-----------------------------"
-
 echo $cyan"3).Melihat animasi kereta"
-
 echo $red"-----------------------------"
-
 echo $cyan"4).Tentang ponsel"
-
 echo $red"-----------------------------"
-
 echo $cyan"5).Ping google.com"
-
 echo $red"-----------------------------"
-
 echo $cyan"6).Spam wa Bukalapak"
-
 echo $red"-----------------------------"
-
 echo $cyan"7).Hack cctv"
-
 echo $red"-----------------------------"
-
 echo $cyan"8).Nuyul kubik"
-
 echo $red"-----------------------------"
-
 echo $cyan"9).LITESPAM"
-
 echo $red"-----------------------------"
-
 echo $cyan"10).SPAMMING"
-
 echo $red"-----------------------------"
-
 echo $cyan"11).Brute force Facebook"
-
 echo $red"-----------------------------"
-
 echo $cyan"12).Bom spam"
-
 echo $red"-----------------------------"
-
 echo $cyan"13).Bot komen Facebook"
-
 echo $red"-----------------------------"
-
 echo $cyan"14).Install metasploit"
-
 echo $red"-----------------------------"
-
 echo $cyan"15).Install osif"
-
 echo $red"-----------------------------"
-
 echo $cyan"16).Scanner inurlbr"
-
 echo $red"-----------------------------"
-
 echo $cyan"17).Install social fish"
-
 echo $red"-----------------------------"
-
 echo $cyan"18).Membuat script deface"
-
 echo $red"-----------------------------"
-
 echo $cyan"19).Install sql map"
-
 echo $red"-----------------------------"
-
 echo ""
-
 read -p"No. Pilihan ===>>" akm;
 
-
 if [ $akm = 1 ] || [ $akm = 1 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 sh toolsddos.sh
-
 fi
-
 
 if [ $akm = 2 ] || [ $akm = 2 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 pkg install moon-buggy
-
 moon-buggy
-
 fi
-
 
 if [ $akm = 3 ] || [ $akm = 3 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 pkg install sl
-
 sl
-
 fi
-
 
 if [ $akm = 4 ] || [ $akm = 4 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 pkg install neofetch
-
 neofetch
-
 fi
-
 
 if [ $akm = 5 ] || [ $akm = 5 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 ping www.google.com
-
 fi
-
 
 if [ $akm = 6 ] || [ $akm = 6 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 pkg install git
-
 pkg install php
-
 git clone https://github.com/DarkCurut08/spam-wa
-
 ls
-
 cd spam-wa
-
 php Moreno77.php
-
 fi
-
 
 if [ $akm = 7 ] || [ $akm = 7 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 apt update && apt upgrade
-
 pkg install python2
-
 pkg install git
-
 pip2 install requests
-
 git clone https://github.com/kancotdiq/ipcs
-
 cd ipcs
-
 python2 scan.py
-
 fi
-
 
 if [ $akm = 8 ] || [ $akm = 8 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 apt update && apt upgrade
-
 pkg install php
-
 pkg install nano
-
 pkg install git
-
 git clone https://github.com/adidoank/kubik
-
 cd kubik
-
 ls
-
 nano cfg.php
-
 fi
-
 
 if [ $akm = 9 ] || [ $akm = 9 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 pkg install git
-
 git clone https://github.com/4L13199/LITESPAM
-
 cd LITESPAM
-
 sh LITESPAM.sh
-
 fi
-
 
 if [ $akm = 10 ] || [ $akm = 10 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 apt update && apt upgrade
-
 apt install git
-
 git clone https://github.com/mrmsdv/sdvspam.git
-
 cd sdvspam
-
 sh install.sh
-
 sh sdv.sh
-
 fi
-
 
 if [ $akm = 11 ] || [ $akm = 11 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 apt update && apt upgrade
-
 apt install python2
-
 apt install python2 -dev
-
 apt install wget
-
 pip2 install mechanize
-
 apt install git
-
 git clone https://github.com/Senitopeng/fbbrutealmu007
-
 cd almu007
-
 ls
-
 python2 MBF.py
-
 fi
-
 
 if [ $akm = 12 ] || [ $akm = 12 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 apt upgrade && apt update
-
 apt install git
-
 git clone https://github.com/Amriez/gcospam
-
 cd gcospam
-
 sh install.sh
-
 sh gco.sh
-
 fi
-
 
 if [ $akm = 13 ] || [ $akm = 13 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 pkg update && pkg upgrade
-
 pkg install git
-
 pkg install python2
-
 pip2 install mechanize 
-
 git clone https://github.com/Senitopeng/botkomena.git
-
 cd botkomena
-
 python2 botkomena.py
-
 fi
-
 
 if [ $akm = 14 ] || [ $akm = 14 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 pkg update && pkg upgrade
-
 pkg install curl
-
 pkg install python
-
 pkg install python2
-
-curl- LO https://raw.githubusercontent.com/Te
-chzindia/Metasploit_For_Termux/master
+curl- LO https://raw.githubusercontent.com/Techzindia/Metasploit_For_Termux/master
 /metasploitTechzindia.sh
-
 ls
-
 chmod 777 metasploitTechzindia.sh
-
 ./metasploitTechzindia.sh
-
 ls
-
 cd metasploit-framework
-
 ./msfconsole
-
 fi
-
 
 if [ $akm = 15 ] || [ $akm = 15 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 apt update && apt upgrade
-
 apt install python2
-
 pip2 install requests mechanize
-
 apt install git
-
 git clone https://github.com/CiKu370/OSIF.git
-
 cd OSIF
-
 pip2 install -r requirements.txt
-
 python2 osif.py
-
-echo $red" Caranya: Pertama masukkan token di bawah, isi username dan pass fb anda, ketikkan help, ketikkan bot(pilih bot terserah anda)
-
+echo $red" Caranya: Pertama masukkan token di bawah, isi username dan pass fb anda, ketikkan help, ketikkan bot(pilih bot terserah anda)"
 fi
-
 
 if [ $akm = 16 ] || [ $akm = 16 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 apt update
-
 apt upgrade
-
 pkg install php
-
 pkg install git
-
 git clone https://github.com/googleinurl/SCANNER-INURLBR.git
-
 cd SCANNER-INURLBR
-
 php inurlbr.php
-
 fi
-
 
 if [ $akm = 17 ] || [ $akm = 17 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 apt update
-
 apt upgrade
-
 pkg install git
-
 pkg install python2
-
 pkg install php
-
 git clone https://github.com/UndeadSec/SocialFish
-
 cd SocialFish
-
 pip install -r requirements.txt
-
 python2 SocialFish.py
-
 fi
 
-
 if [ $akm = 18 ] || [ $akm = 18 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 apt update
-
 apt upgrade
-
 apt install python2
-
 apt install git
-
 git clone https://github.com/Ubaii/script-deface-creator
-
 ls
-
 cd-scrpit-deface-creator
-
 ls
-
 chmod +x create.py
-
 ls
-
 python2 create.py
 
-
 if [ $akm = 19 ] || [ $akm = 19 ]
-
 then
-
 clear
-
 echo $red"_______________________________________________"
-
 echo $green" yyyy   yyyy  GGGGGGGGGGG  mmmmmmmmmmmmmmmm"
-
 echo $green" yyyy   yyyy  GGGG         mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGG   GGGG  mmmm  mmmm  mmmm"
-
 echo $green"        yyyy  GGGG     GG  mmmm  mmmm  mmmm"
-
 echo $green" yyyyyyyyyyy  GGGGGGGGGGG  mmmm  mmmm  mmmm"
-
 echo $red"_______________________________________________"
-
 echo ""
-
 apt update
-
 apt install python
-
 apt install python2
-
 apt install git
-
 git clone https://github.com/sqlmapproject/sqlmap
-
 cd sqlmap
-
 python2 sqlmap.py
-
 fi
 
 
